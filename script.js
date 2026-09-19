839158cf689cf92152b3b4151b6b22085e714091
anagronn <nastasya.vodchits@mail.ru>
2026-09-20 02:26:14 +0300

    feat: implement portfolio block

--- script.js
+++ script.js
@@ -7,3 +7,36 @@ themeToggle.addEventListener('click', () => {
 
   html.dataset.theme = isDark ? 'light' : 'dark';
 });
+
+
+const track = document.querySelector('.slider__track');
+const slides = document.querySelectorAll('.slider__slide');
+
+const prevButton = document.querySelector('.slider__button--prev');
+const nextButton = document.querySelector('.slider__button--next');
+
+let currentSlide = 0;
+
+function updateSlider() {
+  track.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+nextButton.addEventListener('click', () => {
+  currentSlide++;
+
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+
+  updateSlider();
+});
+
+prevButton.addEventListener('click', () => {
+  currentSlide--;
+
+  if (currentSlide < 0) {
+    currentSlide = slides.length - 1;
+  }
+
+  updateSlider();
+});
